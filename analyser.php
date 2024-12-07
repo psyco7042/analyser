657bf9e4379173fb8b0cff66a2a5330c7424f872
psyco7042 <81984378+psyco7042@users.noreply.github.com>
2024-12-07 15:34:16 +0530

    init

---
+++ analyser.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Analyser
+ * Plugin URI:        https://github.com/WordPress-Phoenix/analyser
+ * Description:       Analyse your website's performance
+ * Version:           0.1.0
+ * Author:            Priyam Sengupta
+ * Author URI:        https://github.com/psyco7042
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       analyser
+ * Domain Path:       /languages
+ */
